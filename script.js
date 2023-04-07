6a872f3c6f94431a3bb3ca2b14f6d2740930025d
Sam Ako <elimence@gmail.com>
2023-04-07 12:33:02 +0000

    initial commit

---
+++ script.js
@@ -0,0 +1,11 @@
+const secretText = document.getElementById('secret-text');
+let typedText = '';
+
+document.addEventListener('keydown', (event) => {
+    typedText += event.key.toLowerCase();
+    const targetText = 'i am about to die';
+
+    if (typedText.endsWith(targetText)) {
+        secretText.textContent = 'Welcome in';
+    }
+});
